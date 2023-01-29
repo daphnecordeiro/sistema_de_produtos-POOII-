c9ecef424bb770885bac6f9ba06e2852c37f20cc
Daphne Cordeiro <daphnecordeiro22@gmail.com>
2023-01-29 18:12:59 -0300

    menu fechado

--- Main.java
+++ Main.java
@@ -16,9 +16,9 @@ public class Main {
         //Criar menu escolher perguntar tipo de usuário\
         Locale.setDefault(Locale.US);
         List<Produto> listaProdutos = new ArrayList<>();
-        Mercado mercado =null;
-        Informatica informatica =null;
-        Livro livro= null;
+        Mercado mercado = null;
+        Informatica informatica = null;
+        Livro livro = null;
 
 
         int opcao, opcaoMenuInicial, opcaoSubmenu, opcaoMenuAdm, opcaoMenuCliente;
@@ -27,7 +27,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("-------Menu inicial---------");
-        System.out.println("Digite 1 - sou adm");// Considera que o adm já possui conta e não precisa fazer cadastro
+        System.out.println("Digite 1 - sou adm");
         System.out.println("Digite 2 - sou cliente");
         opcaoMenuInicial = sc.nextInt();
         if (opcaoMenuInicial == 1) {
@@ -60,75 +60,71 @@ public class Main {
                             if (ch == 'm') {
                                 Produto produto = new Produto(nomeProduto, precoProduto, marcaProduto, EnumCategoria.MERCADO);
                                 listaProdutos.add(produto);
-                                System.out.println("Produto adicionado: "+ produto);
-                            } 
-                            else if(ch == 'l') {
+                                System.out.println("Produto adicionado: " + produto);
+                            } else if (ch == 'l') {
                                 Produto produto = new Produto(nomeProduto, precoProduto, marcaProduto, EnumCategoria.LIVRO);
                                 listaProdutos.add(produto);
-                                System.out.println("Produto adicionado: "+ produto);
-                            }  
-                            else if (ch == 'i') {
+                                System.out.println("Produto adicionado: " + produto);
+                            } else if (ch == 'i') {
                                 Produto produto = new Produto(nomeProduto, precoProduto, marcaProduto, EnumCategoria.INFORMATICA);
                                 listaProdutos.add(produto);
-                                System.out.println("Produto adicionado: "+ produto);
+                                System.out.println("Produto adicionado: " + produto);
                             }
                             break;
-                            case 2:
-                            for(Produto produto : listaProdutos){
+                        case 2:
+                            for (Produto produto : listaProdutos) {
                                 System.out.println(produto);
                             }
                             break;
-                            case 3:
+                        case 3:
                             System.out.println("Digite o nome do produto que deseja editar: ");
                             String nome = sc.next();
                             String edicao;
-                            for(Produto produto : listaProdutos){
-                                if(produto.getNome().equals(nome)){
+                            for (Produto produto : listaProdutos) {
+                                if (produto.getNome().equals(nome)) {
                                     System.out.println("Produto encontrado!");
                                     System.out.println(produto);
                                     System.out.print("O que deseja editar? (nome, preco, marca, categoria): ");
                                     edicao = sc.next();
-                                    if(edicao == "nome"){
+                                    if (edicao == "nome") {
                                         System.out.print("Digite o novo nome: ");
                                         String novoNome = sc.next();
                                         produto.setNome(novoNome);
                                     }
-                                    if(edicao == "preco"){
+                                    if (edicao == "preco") {
                                         System.out.print("Digite o novo preco: ");
                                         double novoPreco = sc.nextDouble();
                                         produto.setPreco(novoPreco);
                                     }
-                                    if(edicao == "marca"){
+                                    if (edicao == "marca") {
                                         System.out.print("Digite a nova marca: ");
                                         String novaMarca = sc.next();
                                         produto.setNome(novaMarca);
                                     }
-                                    if(edicao == "categoria"){
+                                    if (edicao == "categoria") {
                                         System.out.print("Digite a nova categoria: (Mercado(m)/Livro(l)/Informática(i))");
-                                            ch = sc.next().charAt(0);
-                                            if (ch == 'm') {
-                                                produto.setCategoria(EnumCategoria.MERCADO);
-                                            } 
-                                            else if(ch == 'l') {
-                                                produto.setCategoria(EnumCategoria.LIVRO);
-                                            }  
-                                            else if (ch == 'i') {
-                                                produto.setCategoria(EnumCategoria.INFORMATICA);
-                                            }
+                                        ch = sc.next().charAt(0);
+                                        if (ch == 'm') {
+                                            produto.setCategoria(EnumCategoria.MERCADO);
+                                        } else if (ch == 'l') {
+                                            produto.setCategoria(EnumCategoria.LIVRO);
+                                        } else if (ch == 'i') {
+                                            produto.setCategoria(EnumCategoria.INFORMATICA);
+                                        }
                                     }
                                     break;
-                                }
-                                else{
+                                } else {
                                     System.out.println("Produto não encontrado!");
                                 }
                                 System.out.println("Produto editado: " + produto);
                             }
                             break;
                     }
-                } while (opcaoMenuAdm != 0);
+                } while (opcaoMenuAdm !=0);
             }
         }
         if (opcaoMenuInicial == 2) {
+            boolean continuar = true;
             do {
                 System.out.println("Digite 1 - Cadastro cliente");
                 System.out.println("Digite 2 - Login cliente");
@@ -136,6 +132,7 @@ public class Main {
 
                 opcaoSubmenu = sc.nextInt();
                 if (opcaoSubmenu == 1) {
+
                     System.out.print("Digite o login a ser cadastrado:");
                     loginCliente = sc.next();
                     System.out.print("Digite a senha a ser cadastrada:");
@@ -154,6 +151,7 @@ public class Main {
                             System.out.println("2 - Visualizar produto");
                             System.out.println("3 - Editar produto");
                             System.out.println("0 - Finalizar o programa");
+
                             System.out.println("Entre com a opção desejada: ");
                             opcaoMenuCliente = sc.nextInt();
                             switch (opcaoMenuCliente) {
@@ -161,13 +159,24 @@ public class Main {
                                     System.out.println("Buscando produto");
                                     break;
                                 case 2:
-                                    System.out.println("Comparando produto, etc");
+                                    System.out.println("Vizualizar produtos");
+                                    break;
+                                case 3:
+                                    System.out.println("Editando Produtos");
+                                    break;
+                                case 0:
+                                    continuar = false;
+                                    System.out.println("Obrigado E Volte Sempre");
+                                    break;
                             }
-                        } while (opcaoMenuCliente !=0);
+                        } while (continuar);
                     }
                 }
-            } while (opcaoMenuInicial!=3);
-
+                if (opcaoSubmenu == 3) {
+                    continuar = false;
+                    System.out.println("Obrigada e volte sempre");
+                }
+            } while (continuar);
         }
     }
 }
